c3194a1f55716717b44fbf914080805385a9f9a4
oreally <o.wartlick@gmail.com>
2025-01-08 15:15:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import streamlit as st
 
-from documents import document_manager
-from bot import chatbot
-
-
-
-def main():
-    st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
     st.markdown(
         """
     <style>
@@ -18,6 +12,12 @@ def main():
         unsafe_allow_html=True,
     )
 
+from documents import document_manager
+from bot import chatbot
+
+
+def main():
+    
     col0, col1, col2, col3, col4 = st.columns([3, 30, 3, 61, 3])
     
     with col1:
